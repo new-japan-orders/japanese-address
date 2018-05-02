66413fa902d7a5d0d949bcbbfccdf232a83e7ef3
emile-yamaji <yamaji_daisuke@1stbit.co.jp>
2018-05-02 03:47:54 +0000

    Changes to be committed: 	new file:   composer.json 	new file:   composer.lock 	new file:   src/ServiceProvider.php 	new file:   src/stub/migrations/create_areas_table.stub 	new file:   src/stub/migrations/create_cities_table.stub 	new file:   src/stub/migrations/create_prefectures_table.stub 	new file:   src/stub/migrations/create_towns_table.stub

---
+++ src/ServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace NewJapanOrders\Scaffold;
+
+use Illuminate\Support\ServiceProvider as Provider;
+
+class ServiceProvider extends Provider {
+
+    /** 
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {   
+        // 
+        $now = date('Y_m_d_His_'); 
+        $this->publishes([
+            __DIR__.'/stub/migrations/create_areas_table.stub' => base_path('database/migrations/'.$now.'create_areas_table.php'),
+            __DIR__.'/stub/migrations/create_prefectures_table.stub' => base_path('database/migrations/'.$now.'create_prefectures_table.php'),
+            __DIR__.'/stub/migrations/create_cities_table.stub' => base_path('database/migrations/'.$now.'create_cities_table.php'),
+            __DIR__.'/stub/migrations/create_towns_table.stub' => base_path('database/migrations/'.$now.'create_towns_table.php'),
+        ]);
+    }   
+
+    /** 
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {   
+        
+    }   
+}
